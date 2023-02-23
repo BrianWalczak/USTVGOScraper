8d81946e30932bb392345f5f1638d4e57ecb225e
BrianWalczak <72092838+BrianWalczak@users.noreply.github.com>
2023-02-23 17:28:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,158 +1,74 @@
-//This code is obfuscated for protection
-const _0x5df628 = _0x5193;
-(function (_0x522220, _0x262b87) {
-    const _0x84438e = _0x5193;
-    const _0x2bad50 = _0x522220();
-    while (!![]) {
-        try {
-            const _0x5e4e2e = parseInt(_0x84438e(0xc6)) / 0x1 * (parseInt(_0x84438e(0xd8)) / 0x2) + parseInt(_0x84438e(0xb9)) / 0x3 * (-parseInt(_0x84438e(0xac)) / 0x4) + parseInt(_0x84438e(0xca)) / 0x5 * (-parseInt(_0x84438e(0xae)) / 0x6) + -parseInt(_0x84438e(0xb3)) / 0x7 + -parseInt(_0x84438e(0xcf)) / 0x8 + -parseInt(_0x84438e(0xd6)) / 0x9 + parseInt(_0x84438e(0xc2)) / 0xa;
-            if (_0x5e4e2e === _0x262b87) {
-                break;
-            } else {
-                _0x2bad50['push'](_0x2bad50['shift']());
-            }
-        } catch (_0x5f0e2d) {
-            _0x2bad50['push'](_0x2bad50['shift']());
-        }
-    }
-}(_0x2a03, 0x71edd));
-const fetch = (..._0x421bba) => import(_0x5df628(0xc3))[_0x5df628(0xbf)](({default: _0x191803}) => _0x191803(..._0x421bba));
-const readline = require(_0x5df628(0xd1));
-const express = require(_0x5df628(0xdb));
-const router = express[_0x5df628(0xd2)]();
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const readline = require('readline');
+const express = require('express');
+const router = express.Router();
 const fs = require('fs');
 const app = express();
-app[_0x5df628(0xcd)](express['static'](__dirname + '/'));
-function _0x2a03() {
-    const _0x20a4f2 = [
-        'writeFile',
-        '16587080SNxleR',
-        'node-fetch',
-        '/get/FoodNetwork<br><br><small>Copyright\x202022,\x20Brian\x20Walczak,\x20All\x20rights\x20reserved.</small></center></html>',
-        '<br>',
-        '16124DjKTcv',
-        '://',
-        'readFile',
-        'includes',
-        '545KByimU',
-        'hls_src=\x27',
-        'protocol',
-        'use',
-        'undefined',
-        '1325704lxWFSc',
-        'USTVGO\x20Scraper\x20is\x20at\x20localhost:3000\x20(port\x203000)',
-        'readline',
-        'Router',
-        '\x22\x20group-title=\x22ustvgo\x22\x20tvg-logo=\x22',
-        'listen',
-        '</html>',
-        '1699569oIfJcJ',
-        'length',
-        '92gsJdAB',
-        '.m3u',
-        'host',
-        'express',
-        '<br><br><br>File\x20Contents:<br>',
-        'status',
-        '<html\x20style=\x27font-family:system-ui\x27>File\x20was\x20saved\x20as\x20',
-        'Copyright\x202022,\x20Brian\x20Walczak,\x20All\x20rights\x20reserved.',
-        'replace',
-        '1724czlwdd',
-        'channels.txt',
-        '43578YyKjBG',
-        '#EXTINF:-1\x20tvg-id=\x22',
-        'redirect',
-        'GET',
-        '\x20|\x20',
-        '718753NZeZQs',
-        'utf8',
-        'split',
-        'params',
-        'log',
-        'providedName',
-        '4767FgLTRV',
-        'get',
-        '.m3u8',
-        '/get/:providedName',
-        'send',
-        ')\x20was\x20not\x20found\x20on\x20USTVGO\x27s\x20directory<br><br>Here\x20is\x20the\x20list\x20of\x20channels\x20to\x20search\x20for:<br>',
-        'then',
-        '<html\x20style=\x27font-family:system-ui\x27><center>To\x20generate\x20an\x20M3U\x20from\x20USTVGO.TV,\x20search\x20for\x20the\x20channel\x20by\x20visiting\x20/get/\x20(It\x20must\x20be\x20the\x20channels\x20code)<br><br>Example:\x20'
-    ];
-    _0x2a03 = function () {
-        return _0x20a4f2;
-    };
-    return _0x2a03();
-}
-app['get']('/', function (_0x27e842, _0x2acc26) {
-    const _0x51e57b = _0x5df628;
-    _0x2acc26['send'](_0x51e57b(0xc0) + (_0x27e842[_0x51e57b(0xcc)] + _0x51e57b(0xc7) + _0x27e842['get']('host')) + _0x51e57b(0xc4));
+app.use(express.static(__dirname + "/"))
+
+app.get('/', function(req, res){
+	res.send(`<html style='font-family:system-ui'><center>To generate an M3U from USTVGO.TV, search for the channel by visiting /get/ (It must be the channels code)<br><br>Example: ${req.protocol + '://' + req.get('host')}/get/FoodNetwork<br><br><small>Copyright 2022, Brian Walczak, All rights reserved.</small></center></html>`)
 });
-app[_0x5df628(0xcd)]('/', router);
-function _0x5193(_0x2c9066, _0x2c744f) {
-    const _0x2a03a2 = _0x2a03();
-    _0x5193 = function (_0x519333, _0x4b2c3c) {
-        _0x519333 = _0x519333 - 0xab;
-        let _0xa72d08 = _0x2a03a2[_0x519333];
-        return _0xa72d08;
-    };
-    return _0x5193(_0x2c9066, _0x2c744f);
-}
-app[_0x5df628(0xba)](_0x5df628(0xbc), function (_0x310969, _0x2847b4) {
-    const _0x1ce169 = _0x5df628;
-    if (_0x310969[_0x1ce169(0xb6)]['providedName'] != null) {
-        var _0x146d23;
-        fs[_0x1ce169(0xc8)](_0x1ce169(0xad), _0x1ce169(0xb4), function (_0x243de0, _0x75228a) {
-            const _0x3be67f = _0x1ce169;
-            var _0x3e6aef = _0x75228a[_0x3be67f(0xb5)]('\x0a');
-            for (var _0x4025fb = 0x0; _0x4025fb < _0x3e6aef['length']; _0x4025fb++) {
-                if (_0x3e6aef[_0x4025fb][_0x3be67f(0xb5)]('\x20|\x20')[0x1] == _0x310969['params'][_0x3be67f(0xb8)]) {
-                    _0x146d23 = _0x3e6aef[_0x4025fb];
-                    name = _0x146d23[_0x3be67f(0xb5)](_0x3be67f(0xb2))[0x0];
-                    code = _0x146d23[_0x3be67f(0xb5)](_0x3be67f(0xb2))[0x1];
-                    logo = _0x146d23[_0x3be67f(0xb5)](_0x3be67f(0xb2))[0x2];
-                    id = _0x146d23[_0x3be67f(0xb5)](_0x3be67f(0xb2))[0x3];
-                    fetch('https://ustvgo.tv/player.php?stream=' + code, {
-                        'method': _0x3be67f(0xb1),
-                        'headers': { 'Referer': 'https://ustvgo.tv/' }
-                    })[_0x3be67f(0xbf)](_0x289797 => _0x289797['text']())['then'](_0x2ee68c => {
-                        const _0x65cc3f = _0x3be67f;
-                        if (_0x2ee68c[_0x65cc3f(0xc9)](_0x65cc3f(0xbb)) == !![]) {
-                            link = _0x2ee68c['split'](_0x65cc3f(0xcb))[0x1][_0x65cc3f(0xb5)]('\x27')[0x0];
-                        } else {
-                            link = 'https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u';
-                        }
-                        m3uTXT = _0x65cc3f(0xaf) + code + _0x65cc3f(0xd3) + logo + '\x22,\x20' + name + '\x0a' + link;
-                        var _0x17c0fa = name['trim']() + _0x65cc3f(0xd9);
-                        fs[_0x65cc3f(0xc1)](_0x17c0fa, m3uTXT, _0x457f92 => {
-                            const _0x42cf14 = _0x65cc3f;
-                            if (_0x457f92)
-                                throw _0x457f92;
-                            _0x2847b4[_0x42cf14(0xbd)](_0x42cf14(0xde) + (_0x310969['protocol'] + _0x42cf14(0xc7) + _0x310969[_0x42cf14(0xba)](_0x42cf14(0xda)) + '/' + _0x17c0fa) + _0x42cf14(0xdc) + m3uTXT + _0x42cf14(0xd5));
-                        });
-                    });
-                }
-            }
-            if (typeof _0x146d23 === _0x3be67f(0xce)) {
-                var _0x487db9;
-                for (var _0x4025fb = 0x0; _0x4025fb < _0x3e6aef[_0x3be67f(0xd7)]; _0x4025fb++) {
-                    _0x487db9 = _0x487db9 + (_0x3e6aef[_0x4025fb]['split'](_0x3be67f(0xb2))[0x1] + '\x20(' + _0x3e6aef[_0x4025fb][_0x3be67f(0xb5)](_0x3be67f(0xb2))[0x0] + ')') + _0x3be67f(0xc5);
-                }
-                _0x2847b4[_0x3be67f(0xbd)]('<html\x20style=\x27font-family:system-ui\x27>The\x20channel\x20that\x20you\x20searched\x20for\x20(' + _0x310969[_0x3be67f(0xb6)][_0x3be67f(0xb8)] + _0x3be67f(0xbe) + _0x487db9[_0x3be67f(0xab)](_0x3be67f(0xce), '') + '</html>');
-            }
-        });
-    }
+app.use('/', router);
+
+app.get('/get/:providedName', function(req, res){
+	if(req.params.providedName != null) {
+	var theChannel;
+	fs.readFile('channels.txt', 'utf8', function(err, channel){
+		var individualChannel = channel.split('\n')
+		
+		for(var i = 0;i < individualChannel.length;i++){
+			if(individualChannel[i].split(' | ')[1] == req.params.providedName) {
+				theChannel = individualChannel[i]
+
+				name = theChannel.split(' | ')[0];
+				code = theChannel.split(' | ')[1];
+				logo = theChannel.split(' | ')[2];
+				id = theChannel.split(' | ')[3];
+
+				fetch(`https://ustvgo.tv/player.php?stream=${code}`, {
+					method: 'GET',
+					headers: {'Referer':'https://ustvgo.tv/'}
+				}).then(response => response.text())
+					.then(src => {
+						if(src.includes('.m3u8') == true) {
+							link = src.split("hls_src='")[1].split("'")[0]
+						}else{
+							link=`https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u`
+						}
+						m3uTXT = `#EXTINF:-1 tvg-id="${code}" group-title="ustvgo" tvg-logo="${logo}", ${name}\n${link}`
+
+						var fileName = `${name.trim()}.m3u`
+						fs.writeFile(fileName, m3uTXT, (err) => {
+							if (err) throw err;
+
+							res.send(`<html style='font-family:system-ui'>File was saved as ${req.protocol + '://' + req.get('host') + '/' + fileName}<br><br><br>File Contents:<br>${m3uTXT}</html>`)
+						}); 
+				});
+			}
+		}
+		
+		if(typeof theChannel === 'undefined') {
+			var channelList;
+			
+			for(var i = 0;i < individualChannel.length;i++){
+				channelList = channelList+`${individualChannel[i].split(' | ')[1]} (${individualChannel[i].split(' | ')[0]})`+"<br>"
+			}
+			res.send(`<html style='font-family:system-ui'>The channel that you searched for (${req.params.providedName}) was not found on USTVGO's directory<br><br>Here is the list of channels to search for:<br>${channelList.replace('undefined', '')}</html>`)
+		}
+	});
+	}
 });
-app['use'](_0x5df628(0xbc), router);
-app[_0x5df628(0xcd)](function (_0x5b10a7, _0x4ab296, _0x2e3267) {
-    const _0x2f777c = _0x5df628;
-    _0x4ab296[_0x2f777c(0xdd)](0x194);
-    _0x4ab296[_0x2f777c(0xb0)]('/');
+app.use('/get/:providedName', router);
+
+app.use(function(req, res, next) {
+    res.status(404);
+    res.redirect('/')
 });
-let server = app[_0x5df628(0xd4)](0xbb8, function () {
-    const _0x24bce8 = _0x5df628;
-    console[_0x24bce8(0xb7)](_0x24bce8(0xd0));
-    console[_0x24bce8(0xb7)](_0x24bce8(0xdf));
+
+let server = app.listen(3000, function(){
+  console.log("USTVGO Scraper is at localhost:3000 (port 3000)");
+	console.log("Copyright 2022, Brian Walczak, All rights reserved.")
 });
 
 //Copyright 2022, Brian Walczak, All rights reserved.
